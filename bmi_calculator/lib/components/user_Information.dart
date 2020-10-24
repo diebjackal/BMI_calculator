05b11fd9c199e7519904ef2e3be7641d011d8527
diebjackal <55783877+diebjackal@users.noreply.github.com>
2020-10-24 21:21:02 +0900

    [ADD] send data
    push data height age weight data

---
+++ bmi_calculator/lib/components/user_Information.dart
@@ -0,0 +1,112 @@
+import 'package:bmi_calculator/utilits/colors.dart';
+import 'package:bmi_calculator/utilits/container_SIze.dart';
+import 'package:bmi_calculator/utilits/constants.dart';
+import 'package:flutter/material.dart';
+
+class ageView extends StatelessWidget {
+  int index;
+  double age;
+  ageView({@required this.index, @required this.age});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: XSizePadding,
+        height: fullSizeContainer,
+        decoration: BoxDecoration(
+          color: colorList[index],
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '20',
+              style: userInformationTextStyle,
+            ),
+            Container(
+              margin: userInformationMSizePadding,
+              child: Text(
+                '살',
+                style: userInformationUnitTextStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class heightView extends StatelessWidget {
+  int index;
+  double height;
+  heightView({@required this.index, @required this.height});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: XSizePadding,
+        height: fullSizeContainer,
+        decoration: BoxDecoration(
+          color: colorList[index],
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '${height.toInt()}',
+              style: userInformationTextStyle,
+            ),
+            Container(
+              margin: userInformationXSizePadding,
+              child: Text(
+                'cm',
+                style: userInformationUnitTextStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class weightView extends StatelessWidget {
+  int index;
+  double weight;
+  weightView({@required this.index, @required this.weight});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: XSizePadding,
+        height: fullSizeContainer,
+        decoration: BoxDecoration(
+          color: colorList[index],
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '${weight.toInt()}',
+              style: userInformationTextStyle,
+            ),
+            Container(
+              margin: userInformationXSizePadding,
+              child: Text(
+                'Kg',
+                style: userInformationUnitTextStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
